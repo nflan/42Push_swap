a92a992a4f6a6413f2c452d0845eeec4e489fe1e
Nicolas FLAN <nflan@e1r8p2.42.fr>
2022-02-09 17:52:10 +0100

    corriger 5 fonctionne plus

--- ft_utils.c
+++ ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: nflan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:27:06 by nflan             #+#    #+#             */
-/*   Updated: 2022/02/04 14:39:36 by nflan            ###   ########.fr       */
+/*   Updated: 2022/02/09 16:03:27 by nflan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int	ft_is_sort(t_begin *begin)
 
 void	ft_push_all_to_a(t_begin *begin)
 {
-	while (begin->pile_b)
+	while (begin->pile_b && begin->bmoves > begin->moves)
 		begin->moves += ft_push(begin, &begin->pile_b, &begin->pile_a, 2);
 }
 
@@ -59,24 +59,21 @@ int	ft_nb_rra(t_pile *pile, t_chunk *chunk, int ind)
 	rra = 0;
 	if (!ctmp || !tmp)
 		return (0);
-	else
+	while (ctmp && ctmp->index != ind)
+		ctmp = ctmp->next;
+	while (tmp)
 	{
-		while (ctmp && ctmp->index != ind)
-			ctmp = ctmp->next;
-		while (tmp)
-		{
-			if (tmp && tmp->num >= ctmp->min && tmp->num <= ctmp->max)
-				nb = tmp->num;
-			tmp = tmp->next;
-		}
-		tmp = pile;
-		while (tmp && tmp->num != nb)
-		{
-			rra++;
-			tmp = tmp->next;
-		}
-		return (ft_lstsize(pile) - rra);
+		if (tmp && tmp->num >= ctmp->min && tmp->num <= ctmp->max)
+			nb = tmp->num;
+		tmp = tmp->next;
 	}
+	tmp = pile;
+	while (tmp && tmp->num != nb)
+	{
+		rra++;
+		tmp = tmp->next;
+	}
+	return (ft_lstsize(pile) - rra);
 }
 
 int	ft_nb_ra(t_pile *pile, t_chunk *chunk, int ind)
